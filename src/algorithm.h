3de54ebd7e83a41224179aad0675207fd205448b
son1k9 <ultrasonica9@gmail.com>
2024-08-22 12:36:23 +0300

    Update algorithm.h

--- src/algorithm.h
+++ src/algorithm.h
@@ -1,18 +1,15 @@
 #pragma once
 #include <functional>
-#include <concepts>
 #include "array.h"
 
-class Algorithm {
-public:
+struct Algorithm {
     using AlgorithmFunction = void(*)(Array&);
-private:
     const std::string name;
     const AlgorithmFunction func;
-public:
+
     Algorithm(AlgorithmFunction func, const std::string& name)
         : name{ name }, func{ func } {}
-    void use(Array& array) {
+    void use(Array& array) const {
         func(array);
     }
 };
